76bc584f4ba950db881506b7291e3eaef2fba5cd
Eduardo Yeves <yevestevez.cg@gmail.com>
2026-04-17 13:44:48 +0100

    Enhance Express app setup with middleware for logging, CORS, and JSON parsing

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,37 @@
 import debug from 'debug';
 import express from 'express';
+import morgan from 'morgan';
+import cors from 'cors';
 import type { Pool } from 'pg';
 
 import { env } from './config/env.ts';
+import { animalsRouter } from './animals/router/animals-router.ts';
 
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const createApp = (pool: Pool) => {
     const log = debug(`${env.PROJECT_NAME}:app`);
     log('Starting Express app...');
     const app = express();
 
-    // pool
+    app.disable('x-powered-by');
+    app.use(morgan('dev'));
+    app.use(
+        cors({
+            origin: '*',
+        }),
+    );
+    app.use(express.json());
+    app.use(express.urlencoded());
+
+    app.use('api/animals', animalsRouter(pool));
+
+    app.use((_req, res) => {
+        res.status(404);
+        res.statusMessage = 'Not Found';
+        res.json({
+            message: 'Resource not found',
+        });
+        return;
+    });
 
     return app;
 };
