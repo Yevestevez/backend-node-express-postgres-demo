7046e1acb6b7ac289ee42190bc0e732695e75ca8
Yevestevez <yevestevez.cg@gmail.com>
2026-04-18 18:44:48 +0200

    Refactor animals router to implement schema validation and improve error handling

--- src/animals/router/animals-router.ts
+++ src/animals/router/animals-router.ts
@@ -3,41 +3,70 @@ import { Router } from 'express';
 import type { Pool } from 'pg';
 
 import { env } from '../../config/env.ts';
-
-// import { PotatoesController } from '../controller/potatoes-controller.ts';
-
-// const router = (controller: AnimalsController) => {
-//     const router = Router();
-//     log('Animals Router created');
-
-//     router.get('/', controller.getAll);
-//     router.get('/:id', controller.getById);
-//     router.post('/', controller.create);
-//     router.patch('/:id', controller.updateById);
-//     router.delete('/:id', controller.deleteById);
-
-//     return router;
-// };
+import {
+    AnimalSchemaDTO,
+    type Animal,
+    type AnimalDTO,
+} from '../schemas/animal.ts';
+import { HttpError } from '../../errors/http-error.ts';
 
 export const animalsRouter = (pool: Pool) => {
     const log = debug(`${env.PROJECT_NAME}:animals-router`);
     log('Starting Animals Router');
 
     const router = Router();
-    log(pool.ending);
 
     router.get('/', async (_req, res) => {
-        return res.json({
-            message: 'List of animals',
-        });
+        const q = `SELECT * FROM animals`;
+        const { rows } = await pool.query<Animal>(q);
+
+        return res.json(rows);
     });
 
     router.get('/:id', async (req, res) => {
         const { id } = req.params;
+        log(id);
+        const q = `SELECT * FROM animals WHERE id = $1`;
 
-        return res.json({
-            message: `Details of animal with id ${id}`,
-        });
+        try {
+            const { rows } = await pool.query<AnimalDTO>(q, [id]);
+
+            return res.json(rows);
+        } catch (error) {
+            const finalError = new HttpError(
+                404,
+                'NotFound',
+                (error as Error).message,
+            );
+            finalError.cause = error;
+            throw finalError;
+        }
+    });
+
+    router.post('/', async (req, res, next) => {
+        const data = AnimalSchemaDTO.parse(req.body);
+        const q = `
+            INSERT INTO animals (name, english_name, sci_name, diet, lifestyle, location, slogan, group_name, image)
+            VALUES
+            (name = $1, english_name = $2, sci_name = $3, diet = $4, lifestyle = $5, location = $6, slogan = $7, group_name = $8, image = $9)
+            RETURNING *
+        `;
+        try {
+            const { rows } = await pool.query(q, [
+                data.name,
+                data.englishName,
+                data.sciName,
+                data.diet,
+                data.lifestyle,
+                data.location,
+                data.slogan,
+                data.group,
+                data.image,
+            ]);
+            return res.json(rows);
+        } catch (error) {
+            next(error);
+        }
     });
 
     return router;
