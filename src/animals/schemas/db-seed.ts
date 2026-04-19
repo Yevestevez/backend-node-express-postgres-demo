c6cd42560dfcbfbf48a91589a48df975ef379c71
Yevestevez <yevestevez.cg@gmail.com>
2026-04-19 08:40:27 +0200

    Refactor animal seed script to change ID type to SERIAL and remove explicit ID insertion

--- src/animals/schemas/db-seed.ts
+++ src/animals/schemas/db-seed.ts
@@ -5,7 +5,7 @@ export const animalSeed = (pool: Pool) => {
 
     pool.query(
         `CREATE TABLE IF NOT EXISTS animals (
-            id VARCHAR(255) PRIMARY KEY,
+            id SERIAL PRIMARY KEY,
             name VARCHAR(255) NOT NULL,
             english_name VARCHAR(255) NOT NULL,
             sci_name VARCHAR(255) NOT NULL,
@@ -20,12 +20,12 @@ export const animalSeed = (pool: Pool) => {
     );
 
     pool.query(`
-        INSERT INTO animals (id, name, english_name, sci_name, diet, lifestyle, location, slogan, group_name, image)
+        INSERT INTO animals (name, english_name, sci_name, diet, lifestyle, location, slogan, group_name, image)
         VALUES 
-        ('1', 'Tigre', 'Tiger', 'Panthera tigris', 'Carnívoro', 'Nocturno', 'Asia', 'El rey de la selva', 'Felinos', 'https://example.com/tiger.jpg'),
-        ('2', 'Elefante', 'Elephant', 'Loxodonta africana', 'Herbívoro', 'Diurno', 'África', 'El gigante gentil de la sabana', 'Proboscídeos', 'https://example.com/elephant.jpg'),
-        ('3', 'Canguro', 'Kangaroo', 'Macropus rufus', 'Herbívoro', 'Diurno', 'Australia', 'El saltarín del outback australiano', 'Marsupiales', 'https://example.com/kangaroo.jpg'),
-        ('4', 'Panda', 'Panda', 'Ailuropoda melanoleuca', 'Herbívoro', 'Diurno', 'China', 'El oso de bambú', 'Ursidae', 'https://example.com/panda.jpg'),
-        ('5', 'Águila', 'Eagle', 'Aquila chrysaetos', 'Carnívoro', 'Diurno', 'Global', 'El rey de los cielos', 'Accipitridae', 'https://example.com/eagle.jpg');
+        ('Tigre', 'Tiger', 'Panthera tigris', 'Carnívoro', 'Nocturno', 'Asia', 'El rey de la selva', 'Felinos', 'https://example.com/tiger.jpg'),
+        ('Elefante', 'Elephant', 'Loxodonta africana', 'Herbívoro', 'Diurno', 'África', 'El gigante gentil de la sabana', 'Proboscídeos', 'https://example.com/elephant.jpg'),
+        ('Canguro', 'Kangaroo', 'Macropus rufus', 'Herbívoro', 'Diurno', 'Australia', 'El saltarín del outback australiano', 'Marsupiales', 'https://example.com/kangaroo.jpg'),
+        ('Panda', 'Panda', 'Ailuropoda melanoleuca', 'Herbívoro', 'Diurno', 'China', 'El oso de bambú', 'Ursidae', 'https://example.com/panda.jpg'),
+        ('Águila', 'Eagle', 'Aquila chrysaetos', 'Carnívoro', 'Diurno', 'Global', 'El rey de los cielos', 'Accipitridae', 'https://example.com/eagle.jpg');
     `);
 };
