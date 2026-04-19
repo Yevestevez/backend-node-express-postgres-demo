05e745fd8c5a98dfc15ac14a0599de424982698c
Yevestevez <yevestevez.cg@gmail.com>
2026-04-19 11:43:05 +0200

    Refactor animal schemas to create a separate update DTO and adjust type inference for DTOs

--- src/animals/schemas/animal.ts
+++ src/animals/schemas/animal.ts
@@ -25,6 +25,18 @@ export const AnimalSchemaDTO = z.object({
     slogan: z.string(),
 });
 
+export const AnimalSchemaUpdateDTO = z.object({
+    name: z.string().nonempty().optional(),
+    englishName: z.string().nonempty().optional(),
+    sciName: z.string().nonempty().optional(),
+    group: z.string().nonempty().optional(),
+    image: z.string().url().optional(),
+    diet: z.string().optional(),
+    lifestyle: z.enum(['Diurno', 'Nocturno']).optional(),
+    location: z.string().optional(),
+    slogan: z.string().optional(),
+});
+
 export type Animal = z.infer<typeof AnimalSchema>;
-export type AnimalDTO = Omit<Animal, 'id'>;
-export type AnimalUpdateDTO = Partial<Omit<Animal, 'id'>>;
+export type AnimalDTO = z.infer<typeof AnimalSchemaDTO>;
+export type AnimalUpdateDTO = z.infer<typeof AnimalSchemaUpdateDTO>;
